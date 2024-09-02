bf875ce7429c341eae7da191a8826d923f57be95
lad <aintstressedanymore@gmail.com>
2024-09-02 19:47:16 +0300

    Ch1.4 END

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("learn-web-servers")
+	mux := http.NewServeMux()
+    
+    server := http.Server {
+        Addr: ":8080",
+        Handler: mux,
+    }
+    log.Fatal(server.ListenAndServe())
 }
