e6693ff1c41a89f9c06cb3708614902316b744cd
lad <aintstressedanymore@gmail.com>
2024-09-03 17:09:56 +0300

    CH1.11

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(".")))
-    mux.Handle("assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("assets"))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        _, err := w.Write([]byte("OK"))
+        if err != nil {
+            fmt.Println("error writing response: ", err)
+        } 
+    })
     
     server := http.Server {
         Addr: ":8080",
